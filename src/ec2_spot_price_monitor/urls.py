b313d75f089716f6fcd499052bbb92148347aaf6
Ron Desmarais <ron.desmarais@gmail.com>
2014-04-08 14:39:20 -0700

    Refactored Code into proper directories, added testcases for client and api, added proper configured logging

--- src/ec2_spot_price_monitor/urls.py
+++ src/ec2_spot_price_monitor/urls.py
@@ -7,7 +7,10 @@ from django.conf.urls import patterns, url
 
 from ec2_spot_price_monitor import views
 
+
 urlpatterns = patterns('',
                       url(r'^$', views.index, name='index'),
                       url(r'^spot_price/', views.request_spot_price, name='request_spot_price'),
 )
+
+views.init_spot_price_service()
--- src/ec2_spot_price_monitor/views.py
+++ src/ec2_spot_price_monitor/views.py
@@ -7,9 +7,9 @@ from pprint import pprint
 from tools import utils
 
 import datetime as dt
-import dateutil.parser
+import os
 
-import boto.ec2,json
+import boto.ec2,json,dateutil.parser,logging
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -17,15 +17,31 @@ from ec2_spot_price_monitor.models import ImageType,InstanceType
 
 from ec2_api_tools import spot_price_api
 
+
+def init_spot_price_service():
+    print("initializing Spot price service ...")
+    
+    config_file = open(os.path.realpath("%s/spot_price_service.cfg"%os.path.dirname(os.path.realpath(__file__))))
+    cfg_file = ""
+    for line in config_file:
+        cfg_file = "%s%s"%(cfg_file,line)
+    cfg_obj = json.loads(cfg_file)
+    
+    logging.basicConfig(filename=cfg_obj['LOG_FILE_NAME'],level=eval(cfg_obj['LOGGING_LEVEL']),format='%(asctime)s %(message)s')
+    logging.info("Initialized spot price service")
+    config_file.close()
+    
+
 @csrf_exempt
 def request_spot_price(request):
-    #get spot price using ec2 tools
-    #conn = boto.connect_ec2()
+    
     jsonMsg = request.POST['jsonMsg']
-    #pprint(jsonMsg)
+    
     json_req = json.loads(jsonMsg)
-    #boto.conn
+    
     json_response_data = []
+    
+    logging.info("Spot Price Requested %s"%json_req)
     
     if json_req['request'] == 'current_spot_price':
         json_req['start_time'] = dt.datetime.now().isoformat()
@@ -45,26 +61,22 @@ def request_spot_price(request):
         stop_date = dateutil.parser.parse(args['job_stop_time'])
         job_exec_time = args['job_exec_time']
         job_period_time = args['job_period_time']
-        #pprint("%s %s %s %s"%(start_date,stop_date,job_exec_time,job_period_time))
         
         curr_date = start_date
         while curr_date < stop_date:
             curr_stop_date = curr_date + dt.timedelta(hours=job_exec_time)
             pprint("comparing ::%s:: ::%s:: ::%s::"%(curr_date,curr_stop_date,stop_date))
-            #get json spot price for period specified
+            
             json_req['start_time'] = curr_date.isoformat()
             json_req['end_time'] = curr_stop_date.isoformat()
             data = spot_price_api.get_current_spot_price(json_req)
-            #for d in data:
-            #    json_response_data.append(d)
+            
             data = utils.filter_response(data, json_req['filter_type'])
             for d in data:
                 json_response_data.append(d)
             curr_date = curr_date + dt.timedelta(hours=job_period_time)
             
-        #json_response_data = utils.filter_response(json_response_data, json_req['filter_type'])    
-                    
-    #json_req_response = {}
+        
     json_req['response']='spot_price'
     if json_req['response_type'] == 'json':
         json_req['response_data']=json_response_data
@@ -76,10 +88,11 @@ def request_spot_price(request):
     return HttpResponse("%s"%(json.dumps(json_req)))
 
 def index(request):
+    logging.info("page request")
     regions = boto.ec2.regions()
     zone_info={}
     for region in regions:
-        pprint("connect to region %s"%region)
+        
         try:
             conn = boto.ec2.connect_to_region(region.name)
             zones = conn.get_all_zones()
@@ -88,13 +101,12 @@ def index(request):
                 zone_array.append(zone.name)
             zone_info[region.name]=zone_array
         except:
-            pprint("Site %s had an issue ignoring"%region.name)
+            
             regions.remove(region)
-    pprint(zone_info)   
-    #get list of image types and vm instance types
+      
     img_types = ImageType.objects.all()
     inst_types = InstanceType.objects.all()
-    pprint(img_types)
+    
     return render(request, 'ec2_spot_price_monitor/SpotPriceService.html',{'regions':regions,'zones':zone_info.items(),'image_types':img_types,'inst_types':inst_types})
     
 
